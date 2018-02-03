d8ab68d574a5b5d19cdfa08dc15031ef83ef4a34
Bruno C. Couto <brunodev02221@gmail.com>
2018-02-03 16:20:52 -0200

    Add version command to CLI

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,13 @@
 #! /usr/bin/env node
 import indexer from "./indexer.js";
+import project from "../package.json";
 
-indexer();
+const args = process.argv.slice(2);
+
+if (args.length === 0) {
+  indexer();
+} else {
+  if (args[0] === "-v" || args[0] === "--version") {
+    console.log(project.version);
+  }
+}
